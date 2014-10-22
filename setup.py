11d4cac2844d8f07e12540285e8b8f69edc40470
Martin Bouma <bouma@egoactive.com>
2014-10-22 22:29:25 +0200

    version bump

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ requires = [
     ]
 
 setup(name='pu_in_content',
-      version="1.0.2",
+      version="1.0.3",
       description='PythonUnited Intranet Content',
       long_description=README + '\n\n' +  CHANGES,
       classifiers=[
